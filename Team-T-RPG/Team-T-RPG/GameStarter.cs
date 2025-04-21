601a465e6c66451036fb0ecb0d93622fb7d16e28
huyn918 <huyn918@gmail.com>
2025-04-21 22:27:08 +0900

    feat. 줄 지우기 기능 개선
    MainFrame에서 작업해둔 거라 미리 커밋해 둡니다.
    - 원하는 만큼 줄 수 지우는 메서드 추가
    - UserInputHandler 사용 시에 알잘딱으로 줄 지워주도록 개선

--- Team-T-RPG/Team-T-RPG/GameStarter.cs
+++ Team-T-RPG/Team-T-RPG/GameStarter.cs
@@ -14,6 +14,64 @@ namespace Team_T_RPG
 
         }
 
+        /*
+            이어하기는 나중에 만들고 ("추후 구현 예정입니다") 안내 띄우기
+            이어하기 선택 시 불러올 데이터를 전부다 Data.cs에 몰아넣고 싶으신 거죠?
+            새로 시작 해서 캐릭터 만들기 들어가기
+        */
 
+        public void StartScene()
+        {
+            while (true)
+            {
+                StartSceneArt();
+                Console.WriteLine("던전 엔 스파르타에 오신 것을 환영합니다.");
+
+                Console.WriteLine("1. 새로 시작");
+                Console.WriteLine("2. 이어하기");
+
+                int userinput = MainFrame.UserInputHandler(1, 2);
+
+                if (userinput == 1)
+                {
+                    Console.Clear();
+                    NameCharacter();
+                    ChooseJob();
+                    break;
+
+                }
+                else if (userinput == 2) // 추후 이어하기 구현 시 여기서 다 불러오고 break
+                {
+                    Console.WriteLine("저장 기능은 미구현입니다. 죄송합니다.\n잠시 후 시작화면으로 돌아갑니다.");
+                    Thread.Sleep(1500); // 1.5초 대기
+                    Console.Clear();
+                }
+            }
+        } // 시작 시 새로하기 or 이어하기 선택창 (이어하기는 추후 구현) + 캐릭터 생성까지.
+
+        public void NameCharacter()
+        {
+            while (true)
+            {
+                Console.WriteLine("캐릭터를 생성합니다. \n닉네임을 입력하세요. (10자 이하)");
+                string userinput = Console.ReadLine();
+
+                if ((userinput?.Length ?? 0) > 10 || (userinput?.Length ?? 0) == 0) // ?. 연산자와 ?? 연산자 활용해서 null에러 방지
+                {
+                    MainFrame.ClearConsoleLine(4); // 입력한 줄 지워버리고 아래 출력
+                    Console.WriteLine("닉네임이 너무 짧거나 깁니다. 다시 입력하세요.");
+                }
+                else
+                {
+                    Data.Name = userinput;
+                    break;
+                }
+            }
+        }
+
+        public void ChooseJob()
+        {
+            Console.WriteLine("아무튼 직업 선택");
+        }
     }
 }
--- Team-T-RPG/Team-T-RPG/MainFrame.cs
+++ Team-T-RPG/Team-T-RPG/MainFrame.cs
@@ -12,14 +12,15 @@ namespace Team_T_RPG
         static void Main()
         {
             
-            StartScene(); // 게임 시작, 이어하기 및 캐릭터 생성 (이어하기는 추후 구현)
+            GameStarter gameStarter = new GameStarter(); // 게임스타터 불러오기 : 게임 시작, 캐릭터 생성 및 저장 데이터 불러오기
+            gameStarter.StartScene();
 
-            while (true) // 아래 조건 미충족 시 break
+            while (true) // 게임오버 조건 미달성 시 무한 반복
 
             {
                 PayTax(); // 세금 납부
 
-                if (data.Level == 0) // 체력 == 0 || 세금을 내고 골드가 음수값이 된 경우 게임오버 처리
+                if (Data.Hp == 0 || Data.Money <= 0) // 체력 == 0 || 세금을 내고 골드가 음수값이 된 경우 게임오버
                     break;
                 else
                 {
@@ -31,45 +32,33 @@ namespace Team_T_RPG
             // 사인을 넣고 싶으면 다 하고 추후 기능 구현하기!
 
         }
-       
 
-        public static void StartScene()
+
+
+        
+        public static int UserInputHandler(int min, int max, int failcount = 0)
         {
-            GameStarter gameStarter = new GameStarter();
-            
-            gameStarter.StartSceneArt();
-            Console.WriteLine("던전 엔 스파르타에 오신 것을 환영합니다.");
-
-            Console.WriteLine("1. 새로 시작");
-            Console.WriteLine("2. 이어하기");
-
-            int userinput = UserInputHandler(1,2);
-            
-
-            /*
-             이어하기는 나중에 만들고 ("추후 구현 예정입니다") 안내 띄우기
-             이어하기 선택 시 불러올 데이터를 전부다 Data.cs에 몰아넣고 싶으신 거죠?
-             새로 시작 해서 캐릭터 만들기 들어가기
-            */
-        } // 시작 시 새로하기 or 이어하기 선택창 (이어하기는 추후 구현) + 캐릭터 생성까지.
-        public static int UserInputHandler(int min, int max) // min,max값 사잇값 받아주는 메서드
-        {
+            Console.WriteLine("--------------------------------");
             Console.WriteLine("원하는 행동을 선택하세요.");
-
+           
             string userInput = Console.ReadLine();
             bool isVaildInput = Int32.TryParse(userInput, out int result);
-
+            
             if (isVaildInput && result >= min && result <= max)
             {
                 return result;
             }
             else
             {
+                int isFailAgain = (failcount == 0) ? 0 : 1; // 첫 실행 때만 0이고 다음 루프부터는 1
+                ClearConsoleLine(3 + isFailAgain);
+                failcount++;
                 Console.WriteLine("잘못된 입력입니다. 다시 입력해주세요");
-                return UserInputHandler(min, max);
+                return UserInputHandler(min, max, failcount);
             }
-
         }
+        // min,max값 사잇값 입력 받아서 int로 리턴해줌. 호출할 때는 최소 최댓값만 넣어주세요! UserInputHandler(1,3) 이런 식으로
+        // 줄 수 지워주는 함수 추가해서 이제 매번 새로 프린트할 필요 없이 커서가 아래서 놉니다.
 
         public static void PayTax() // 세금 계속 루프돌때마다 세금내면 곤란하니 bool값 써서 Day 변동이 있을 시만 걷기
         {
@@ -115,5 +104,25 @@ namespace Team_T_RPG
 
 
         }
+
+        
+        public static void ClearConsoleLine(int howmanyline)
+        {
+            // 현재 커서 위치 저장
+            int currentLine = Console.CursorTop;
+
+            // 지우려는 줄 수가 현재 줄보다 크지 않게 제한 : 커서가 1줄에 있는데 3줄 지운다고 하면 에러 나니까 1줄로 강제 내림
+            howmanyline = Math.Min(howmanyline, currentLine);
+
+            for (int i = 0; i < howmanyline; i++)
+            {
+                int lineToClear = currentLine - i - 1;
+                Console.SetCursorPosition(0, lineToClear);
+                Console.Write(new string(' ', Console.WindowWidth));
+            }
+
+            Console.SetCursorPosition(0, currentLine - howmanyline);
+        }
+        // 입력한 줄 수만큼 아래부터 콘솔라인 지워 줍니다. 편하게 호출해서 쓰세요. ClearConsoleLine(5) 이러면 5줄 지워줌.
     }
 }
