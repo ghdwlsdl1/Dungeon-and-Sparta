2a6039731f9770d07294f7ce3ce1d45950b911f4
YuJHun <76084438+YuJHun@users.noreply.github.com>
2025-04-22 17:41:44 +0900

    솔루션에 Stats 추가

--- Team-T-RPG/Team-T-RPG/Stats.cs
+++ Team-T-RPG/Team-T-RPG/Stats.cs
@@ -4,148 +4,204 @@ namespace Team_T_RPG
 {
     public class Stats
     {
-        // 레벨을 업데이트하는 메서드
-        public void UpdateLevel()
+        public  void UpdateLevel()
         {
-            int experienceMax = 10 * Level * Level;  // 최대 경험치 계산
-            if (experience >= experienceMax)  // 경험치가 최대 경험치를 초과하면
+            int experienceMax = 10 * Data.Level * Data.Level;
+            if (Data.experience >= experienceMax)
             {
-                Level++;  // 레벨 상승
-                experience -= experienceMax;  // 초과한 경험치를 남겨둠
+                Data.Level++;
+                Data.experience -= experienceMax;
             }
         }
-
-        // 최대 HP를 업데이트하는 메서드
-        public void UpdateHpMax()
+        public  void UpdateHpMax()
         {
-            HpMax = Con * 2;  // 최대 HP는 체력(Con) * 2
-            if (Hp > HpMax)  // 
+            Data.HpMax = Data.Con * 2;  // 최대 HP는 체력(Con) * 2
+            if (Data.Hp > Data.HpMax)  // 최대 HP가 현재 HP를 초과하면
             {
-                Hp = HpMax;  // HP를 최대 HP로 설정
+                Data.Hp = Data.HpMax;  // HP를 최대 HP로 설정
             }
         }
-
         // 최대 MP를 업데이트하는 메서드
-        public void UpdateMpMax()
+        public  void UpdateMpMax()
         {
-            MpMax = Wis * 2;  // 최대 MP는 지능(Wis) * 2
-            if (Mp > MpMax)  // 
+            Data.MpMax = Data.Wis * 2;  // 최대 MP는 지능(Wis) * 2
+            if (Data.Mp > Data.MpMax)  // 최대 MP가 현재 MP를 초과하면
             {
-                Mp = MpMax;  // MP를 최대 MP로 설정
+                Data.Mp = Data.MpMax;  // MP를 최대 MP로 설정
             }
         }
-
         // 힘(Strength)을 업데이트하는 메서드
-        public void UpdateStr()
+        public  void UpdateStr()
         {
             int weaponBonus = 0;
-            if (weaponEquip[3])  // 무기가 장착되어 있으면
+            int weaponEquipNum = GetEquippedWeaponIndex();
+
+            if (weaponEquipNum >= 0)
             {
-                weaponBonus = weaponStats[3];  // 무기에서 제공하는 보너스 추가
+                weaponBonus = Data.weaponStats[weaponEquipNum];
             }
 
-            Str = startStr + weaponBonus + Level;  // 기본 힘 + 무기 보너스 + 레벨
+
+            Data.Str = Data.startStr + weaponBonus + Data.Level;  // 기본 힘 + 무기 보너스 + 레벨
         }
 
         // 민첩(Dexterity)을 업데이트하는 메서드
-        public void UpdateDex()
+        public  void UpdateDex()
         {
-            int weaponBonus = 0;
-            if (weaponEquip[4])  // 무기가 장착되어 있으면
-            {
-                weaponBonus += weaponStats[4];  // 무기에서 제공하는 보너스를 추가
-            }
-
-            Dex = startDex + weaponBonus + Level;  // 기본 민첩 + 무기 보너스 + 레벨
+            int weaponBonus = GetWeaponBonus();
+            Data.Dex = Data.startDex + weaponBonus + Data.Level;  // 기본 민첩 + 무기 보너스 + 레벨  int weaponEquipNum = 0;
         }
 
         // 지능(Intelligence)을 업데이트하는 메서드
-        public void UpdateInt()
+        public  void UpdateInt()
         {
-            int weaponBonus = 0;
-            if (weaponEquip[5])  // 무기가 장착되어 있으면
-            {
-                weaponBonus += weaponStats[5];  // 무기에서 제공하는 보너스를 추가
-            }
-
-            Int = startInt + weaponBonus + Level;  // 기본 지능 + 무기 보너스 + 레벨
+            int weaponBonus = GetWeaponBonus();
+            Data.Int = Data.startInt + weaponBonus + Data.Level;  // 기본 지능 + 무기 보너스 + 레벨
         }
 
         // 체력(Constitution)을 업데이트하는 메서드
-        public void UpdateCon()
+        public  void UpdateCon()
         {
             int assistBonus = 0;
-            if (assistEquip[5])  // 보조 장비가 장착되어 있으면
+            if (Data.assistEquip[5])  // 보조 장비가 장착되어 있으면
             {
-                assistBonus += assistStats[5];  // 보조 장비에서 제공하는 보너스 추가
+                assistBonus += Data.assistStats[5];  // 보조 장비에서 제공하는 보너스 추가
             }
 
-            Con = startCon + assistBonus + Level;  // 기본 체력 + 보조 장비 보너스 + 레벨
+            Data.Con = Data.startCon + assistBonus + Data.Level;  // 기본 체력 + 보조 장비 보너스 + 레벨
         }
 
         // 지혜(Wisdom)을 업데이트하는 메서드
-        public void UpdateWis()
+        public  void UpdateWis()
         {
             int assistBonus = 0;
-            if (assistEquip[4])  // 보조 장비가 장착되어 있으면
+            if (Data.assistEquip[4])  // 보조 장비가 장착되어 있으면
             {
-                assistBonus += assistStats[4];  // 보조 장비에서 제공하는 보너스 추가
+                assistBonus += Data.assistStats[4];  // 보조 장비에서 제공하는 보너스 추가
             }
-            Wis = startWis + assistBonus + Level;  // 기본 지혜 + 보조 장비 보너스 + 레벨
+            Data.Wis = Data.startWis + assistBonus + Data.Level;  // 기본 지혜 + 보조 장비 보너스 + 레벨
         }
 
         // 행운(Luck)을 업데이트하는 메서드
-        public void UpdateLuk()
+        public  void UpdateLuk()
         {
             int assistBonus = 0;
-            if (assistEquip[3])  // 보조 장비가 장착되어 있으면
+            if (Data.assistEquip[3])  // 보조 장비가 장착되어 있으면
             {
-                assistBonus += assistStats[3];  // 보조 장비에서 제공하는 보너스 추가
+                assistBonus += Data.assistStats[3];  // 보조 장비에서 제공하는 보너스 추가
             }
-            Luk = startLuk + assistBonus;  // 기본 행운 + 보조 장비 보너스
+            Data.Luk = Data.startLuk + assistBonus;  // 기본 행운 + 보조 장비 보너스
         }
 
         // 공격력(Attack)을 업데이트하는 메서드
-        public void UpdateAtk()
+        public  void UpdateAtk()
         {
             int weaponBonus = 0;
 
-            for (int i = 0; i < weaponEquip.Length; i++)  // 모든 무기 장착 여부를 확인
+            for (int i = 0; i < Data.weaponEquip.Length; i++)  // 모든 무기 장착 여부를 확인
             {
-                if (weaponEquip[i])  // 무기가 장착되어 있으면
+                if (Data.weaponEquip[i])  // 무기가 장착되어 있으면
                 {
-                    weaponBonus += weaponAtk[i];  // 무기에서 제공하는 공격력 추가
+                    weaponBonus += Data.weaponAtk[i];  // 무기에서 제공하는 공격력 추가
                 }
             }
 
-            Atk = 1 + Str + weaponBonus;  // 기본 공격력 + 힘 + 무기 보너스
+            Data.Atk = 1 + Data.Str + weaponBonus;  // 기본 공격력 + 힘 + 무기 보너스
         }
-
         // 방어력(Defense)을 업데이트하는 메서드
-        public void UpdateDef()
+        public  void UpdateDef()
         {
             int assistBonus = 0;
             int armorBonus = 0;
 
-            for (int i = 0; i < assistEquip.Length; i++)  // 모든 보조 장비 확인
+            for (int i = 0; i < Data.assistEquip.Length; i++)  // 모든 보조 장비 확인
             {
-                if (assistEquip[i])  // 보조 장비가 장착되어 있으면
+                if (Data.assistEquip[i])  // 보조 장비가 장착되어 있으면
                 {
-                    assistBonus += assistDef[i];  // 보조 장비에서 제공하는 방어력 추가
+                    assistBonus += Data.assistDef[i];  // 보조 장비에서 제공하는 방어력 추가
                 }
             }
-            for (int i = 0; i < armorEquip.Length; i++)  // 모든 방어구 확인
+            for (int i = 0; i < Data.armorEquip.Length; i++)  // 모든 방어구 확인
             {
-                if (armorEquip[i])  // 방어구가 장착되어 있으면
+                if (Data.armorEquip[i])  // 방어구가 장착되어 있으면
                 {
-                    armorBonus += armorDef[i];  // 방어구에서 제공하는 방어력 추가
+                    armorBonus += Data.armorDef[i];  // 방어구에서 제공하는 방어력 추가
                 }
             }
-            Def = Con / 3 + armorBonus + assistBonus;  // 체력의 1/3 + 방어구 보너스 + 보조 장비 보너스
+            Data.Def = Data.Con / 3 + armorBonus + assistBonus;  // 체력의 1/3 + 방어구 보너스 + 보조 장비 보너스
         }
+        public  int GetEquippedWeaponIndex()
+        {
+            for (int i = 0; i < Data.weaponEquip.Length; i++)
+            {
+                if (Data.weaponEquip[i])//장착한 무기는 true해뒀기에 true를 찾는다
+                {
+                    return i; // 첫 번째 true인 인덱스를 반환
+                }
+            }
+            return -1; // 장착된 무기가 없으면 -1 반환
+        }
+        public  int GetWeaponBonus()
+        {
+            int weaponEquipNum = GetEquippedWeaponIndex();
 
+            if (weaponEquipNum >= 0)
+            {
+                return Data.weaponStats[weaponEquipNum];
+            }
+
+            return 0; // 장착된 무기가 없으면 보너스는 0
+        }
+        public  void testInven()
+        {
+            for (int i = 0; i < Data.weapon.Length; i++)
+            {
+                if (Data.weaponTf[i] >= 0)
+                {
+                    string equippedMark = Data.weaponEquip[i] ? " (장착중)" : "";
+                    Console.WriteLine($"{i}. {Data.weapon[i]} 소지 갯수: {Data.weaponTf[i]}  공격력: {Data.weaponAtk[i]}  스탯+: {Data.weaponStats[i]}{equippedMark}");
+                }
+            }
+            Console.Write("\n장착할 무기 번호를 입력하세요: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int selectedIndex))
+            {
+                if (selectedIndex > 0 && selectedIndex < Data.weapon.Length && Data.weaponTf[selectedIndex] > 0)
+                {
+                    // 기존 무기 해제
+                    for (int i = 0; i < Data.weaponEquip.Length; i++)
+                    {
+                        Data.weaponEquip[i] = false;
+                    }
+
+                    // 선택한 무기 장착
+                    Data.weaponEquip[selectedIndex] = true;
+
+                    Console.WriteLine($"\n▶ [{Data.weapon[selectedIndex]}]를 장착했습니다!");
+                }
+                else if (selectedIndex == 0)
+                {
+                    for (int i = 0; i < Data.weaponEquip.Length; i++)
+                    {
+                        Data.weaponEquip[i] = false;
+                    }
+                    Data.weaponEquip[0] = true;
+                    Console.WriteLine($"\n▶ [{Data.weapon[selectedIndex]}]를 장착했습니다!");
+
+                }
+                else
+                {
+                    Console.WriteLine("※ 잘못된 선택이거나 해당 무기를 소지하고 있지 않습니다.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("※ 숫자를 입력해주세요.");
+            }
+        }
         // 모든 스탯을 업데이트하는 메서드
-        public void UpdateStats()
+        public  void UpdateStats()
         {
             UpdateLevel();  // 레벨 업데이트
 
