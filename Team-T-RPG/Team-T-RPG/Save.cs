483d220a6181330dbee7fdd5e60a3f4b177af906
huyn918 <huyn918@gmail.com>
2025-04-25 11:52:34 +0900

    WIP save

---
+++ Team-T-RPG/Team-T-RPG/Save.cs
@@ -0,0 +1,178 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Team_T_RPG
+{
+        public class TempData
+        {
+            public string Name = ""; // 이름
+            public int JobNames = 0; // 직업 선택 번호
+            public string[] Job = { "없음", "전사", "도적", "마법사", "야만인" };// 직업이름
+            public int Hp = 0; // 현재 HP
+            public int HpMax = 0;// 최대 HP
+            public int Mp = 0; // 현재 MP
+            public int MpMax = 0; //최대 MP
+            public int Level = 1; //레벨 기초 값1
+            public int experience = 0; //경험치
+
+            public int Str = 0;  // 힘
+            public int Dex = 0;  // 민첩
+            public int Int = 0;  // 지능
+            public int Con = 0;  // 건강
+            public int Wis = 0;  // 지혜
+            public int Luk = 0;  // 운
+            public int startStr = 0;  // 힘
+            public int startDex = 0;  // 민첩
+            public int startInt = 0;  // 지능
+            public int startCon = 0;  // 건강
+            public int startWis = 0;  // 지혜
+            public int startLuk = 0;  // 운
+
+            public int Atk = 0; //공격력
+            public int Def = 0; // 방어력
+
+            public int Money = 2000000000;
+            //====================마을 시스템====================
+            public bool duty = false;
+            public int Day = 1;
+            //====================아이탬====================
+            public string[] weapon = { "없음", "롱소드", "단검", "지팡이", "도끼", "천상의 검", "밤의 송곳니", "빛의 지팡이", "파멸의 도끼" };// 아이탬 이름
+            public int[] weaponTf = { 0, 0, 0, 0, 0, 0, 0, 0, 0 }; // 소지 여부
+            public int[] weaponEquip = { 0, 0, 0, 0, 0, 0, 0, 0, 0 }; // 장착 여부
+            public int[] weaponAtk = { 0, 5, 5, 7, 3, 2, 0, 0, 0 }; // 추가 공격력
+                                                                           //public static int[] weaponStats = { 0, 0, 0, 5, 5, 7, 0, 0, 0 }; // 추가 스텟
+            public int[][] weaponStats = new int[][] {
+                    //힘 ,민첩, 지능
+            new int[] { 0,  0,  0 },//없음
+            new int[] { 0,  0,  0 },//롱소드
+            new int[] { 0,  0,  0 },//단검
+            new int[] { 0,  0,  5 },//지팡이
+            new int[] { 5,  0,  0 },//도끼
+            new int[] { 0,  5,  0 },//천상의 검
+            new int[] { 0,  7,  0 },//밤의 송곳니
+            new int[] { 0,  0,  7 },//빛의 지팡이
+            new int[] { 7,  0,  0 }//파멸의 도끼
+             };
+            public int[] weaponDeal = { 0, 1000, 1000, 1000, 1000, 1000, 5000, 5000, 5000, 5000 }; // 금액
+
+            public string[] assist = { "없음", "히터 실드", "행운의 부적", "빛나는 반지", "도바킨 투구", "태양 목걸이" };
+            public int[] assistTf = { 0, 0, 0, 0, 0, 0 };
+            public int[] assistEquip = { 0, 0, 0, 0, 0, 0 };
+            public int[] assistDef = { 0, 1, 2, 0, 0, 0, }; // 추가 방어력
+                                                            //public static int[] assistStats = { 0, 0, 0, 3, 5, 5 }; // 추가 스텟
+            public int[][] assistStats = new int[][] {
+                    //건강,지혜,운
+            new int[] { 0,  0,  0 },//없음
+            new int[] { 0,  2,  0 },//히터 실드
+            new int[] { 0,  0,  5 },//행운의 부적
+            new int[] { 0,  3,  0 },//빛나는 반지
+            new int[] { 2,  0,  0 },//도바킨 투구
+            new int[] { 5,  0,  0 },//태양 목걸이
+            };
+            public string[] armor = { "없음", "낡은 갑옷", "가죽 갑옷", "사슬 갑옷", "강철 갑옷", "판금 갑옷" };
+            public int[] armorTf = { 0, 0, 0, 0, 0, 0 };
+            public int[] armorEquip = { 0, 0, 0, 0, 0, 0 };
+            public int[] armorDef = { 0, 1, 3, 5, 7, 10 }; // 추가 방어력
+            public int[] armorDeal = { 0, 1000, 2000, 3000, 4000, 5000 }; // 금액
+
+            public string[] potion = { "없음", "체력 물약", "상처약", "고급 상처약", "마나 물약", "파워 엘릭서" };
+            public int[] potionTf = { 0, 0, 0, 0, 0, 0 };
+            public int[] potionMp = { 0, 0, 0, 0, 50, 20 };//MP
+            public int[] potionHp = { 0, 10, 30, 50, 0, 40 }; // 추가 체력은 회복이라고 생각했습니다.
+            public int[] potionDeal = { 0, 10, 20, 30, 40, 50 }; // 금액 
+            //====================몬스터====================
+
+            public string[] monster = { "고블린", "오크", "오우거", "리치" };
+            public int[] monster_drop_weapon_index = { 2, 1, 3, 4 };
+            public int[] msAtk = { 10, 10, 10, 15 }; // 기본 공격력
+            public int[] msHp = { 30, 50, 70, 30 }; // 기본 체력
+            public int[] msDex = { 15, 10, 5, 10 }; // 기본 이속
+
+            //====================주사위====================
+            public Random random = new Random(); // static으로 변경
+
+            public int dice20() // 20면 주사위
+            {
+                return random.Next(1, 21) + Luk;
+            }
+
+            public int dice6() // 6면 주사위
+            {
+                return random.Next(1, 7);
+            }
+            //====================던전 시스템====================
+            public int dungeonDay = 3;   //날짜
+            public int dungeonHour = 0;   //시간
+            //====================던전 맵====================
+            public int playerX = -1; //플레이어좌표x
+            public int playerY = -1;
+            public int portalX = -2; //포탈좌표
+            public int portalY = -2;
+            public int treasureX = -1; //보물좌표
+            public int treasureY = -1;
+            public int floor = 1; //층수
+            public bool floorChange = false; //층변경감지
+            public char[,] map; //맵
+            public List<(int x, int y)> monsterPositions = new List<(int x, int y)>();
+            public int monsterTurn = 0; //몬스터 행동
+            public int tired = 0;
+            public int ultimate = 0;
+        }
+
+
+    public static class SaveSystem
+    {
+        public static void Save()
+        {
+            
+            TempData temp = new TempData();
+            CopyStaticToInstance<Data, TempData>(temp);
+            string json = JsonSerializer.Serialize(temp);
+            File.WriteAllText(path, json);
+        }
+
+        public static void Load()
+        {
+            string json = File.ReadAllText(path);
+            TempData loaded = JsonSerializer.Deserialize<TempData>(json);
+            Copy(loaded, Data);
+        }
+
+        public static void CopyStaticToInstance<TStatic, TInstance>(TInstance target)
+        {
+            var staticFields = typeof(TStatic).GetFields(BindingFlags.Static | BindingFlags.Public);
+            var instanceFields = typeof(TInstance).GetFields(BindingFlags.Instance | BindingFlags.Public);
+
+            foreach (var staticField in staticFields)
+            {
+                var targetField = instanceFields.FirstOrDefault(f => f.Name == staticField.Name && f.FieldType == staticField.FieldType);
+                if (targetField != null)
+                {
+                    targetField.SetValue(target, staticField.GetValue(null)); // static이라 객체 인스턴스는 null
+                }
+            }
+        }
+
+        public static void CopyInstanceToStatic<TInstance, TStatic>(TInstance source)
+        {
+            var instanceFields = typeof(TInstance).GetFields(BindingFlags.Instance | BindingFlags.Public);
+            var staticFields = typeof(TStatic).GetFields(BindingFlags.Static | BindingFlags.Public);
+
+            foreach (var instanceField in instanceFields)
+            {
+                var targetField = staticFields.FirstOrDefault(f => f.Name == instanceField.Name && f.FieldType == instanceField.FieldType);
+                if (targetField != null)
+                {
+                    targetField.SetValue(null, instanceField.GetValue(source)); // static이라 객체 인스턴스는 null
+                }
+            }
+        }
+    }
+
+}
